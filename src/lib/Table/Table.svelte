337221349e06bfc1cf59b3185b40f61198687e84
kateyukhnik <139224932+kateyukhnik@users.noreply.github.com>
2025-10-14 15:29:28 +0300

    [publish]

--- src/lib/Table/Table.svelte
+++ src/lib/Table/Table.svelte
@@ -231,18 +231,20 @@
                     }}
                     aria-label="Копировать текст"
                   >
-                    {#if copiedCell.y === index && copiedCell.x === column.key}
-                      <div class="rounded-md bg-[var(--green-color)] px-2 py-1 text-sm shadow-lg">✓</div>
-                    {:else}
-                      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-                        <g fill="none" stroke="currentColor" stroke-width="1.5">
-                          <path
-                            d="M6 11c0-2.828 0-4.243.879-5.121C7.757 5 9.172 5 12 5h3c2.828 0 4.243 0 5.121.879C21 6.757 21 8.172 21 11v5c0 2.828 0 4.243-.879 5.121C19.243 22 17.828 22 15 22h-3c-2.828 0-4.243 0-5.121-.879C6 20.243 6 18.828 6 16z"
-                          />
-                          <path d="M6 19a3 3 0 0 1-3-3v-6c0-3.771 0-5.657 1.172-6.828S7.229 2 11 2h4a3 3 0 0 1 3 3" />
-                        </g>
-                      </svg>
-                    {/if}
+                    <div class=" size-5 text-sm [&_svg]:h-full [&_svg]:max-h-full [&_svg]:w-full [&_svg]:max-w-full">
+                      {#if copiedCell.y === index && copiedCell.x === column.key}
+                        <div class="rounded-md bg-[var(--green-color)] shadow-lg">✓</div>
+                      {:else}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                          <g fill="none" stroke="currentColor" stroke-width="1.5">
+                            <path
+                              d="M6 11c0-2.828 0-4.243.879-5.121C7.757 5 9.172 5 12 5h3c2.828 0 4.243 0 5.121.879C21 6.757 21 8.172 21 11v5c0 2.828 0 4.243-.879 5.121C19.243 22 17.828 22 15 22h-3c-2.828 0-4.243 0-5.121-.879C6 20.243 6 18.828 6 16z"
+                            />
+                            <path d="M6 19a3 3 0 0 1-3-3v-6c0-3.771 0-5.657 1.172-6.828S7.229 2 11 2h4a3 3 0 0 1 3 3" />
+                          </g>
+                        </svg>
+                      {/if}
+                    </div>
                   </button>
                 {/if}
               {/if}
