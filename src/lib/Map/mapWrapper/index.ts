fcb97cabf3e2a669f52e2449f1199a6a2fef5a5a
kateyukhnik <139224932+kateyukhnik@users.noreply.github.com>
2026-01-28 09:56:19 +0300

    [deploy] [publish]

--- src/lib/Map/mapWrapper/index.ts
+++ src/lib/Map/mapWrapper/index.ts
@@ -6,3 +6,5 @@ export { default as FullScreenControl } from "./controls/FullScreenControl.svelt
 export { default as Marker } from "./Marker.svelte"
 export { default as Popup } from "./Popup.svelte"
 export { default as CustomControl } from "./controls/CustomControl.svelte"
+
+export { getMapContext, getSourceContext, getMarkerContext } from "./contexts.svelte.js"
